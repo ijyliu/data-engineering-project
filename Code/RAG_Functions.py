2abd20f2b21afc3ccbd0bf7faff68e94a98e7178
Isaac Liu <ijyliu@uchicago.edu>
2024-04-11 09:31:23 -0700

    working top5 retrieval

--- Code/RAG_Functions.py
+++ Code/RAG_Functions.py
@@ -20,12 +20,31 @@ def get_mixedbread_of_query(model, input: str):
     transformed_input = transform_query(input)
     return model.encode(transformed_input)
 
-def return_top_5_sentences(client, query_embedding):
-    res = client.search(
-        collection_name="text_embeddings",
-        data=[query_embedding],
-        limit=5, # Max. number of search results to return
-        search_params={"metric_type": "COSINE", "params": {}}, # Search parameters
-        output_fields = ["sentence", "company_name", "document_name"] # Output fields
+def return_top_5_sentences(collection, query_embedding):
+    search_params = {
+        "metric_type": "L2", # simliarity metric
+        "offset": 0, # the number of top-k hits to skip
+        "ignore_growing": False#, # ignore growing segments
+        #"params": {"nprobe": 10} # number of cluster units, only used for some index types
+    }
+    results = collection.search(
+        data=[query_embedding], # query vector
+        anns_field="embedding", # name of field to search on
+        param=search_params, # seach parameters set above
+        limit=5,# num results to return
+        expr=None, # boolean filter
+        output_fields=['company_name', 'sentence'], # fields to return 
+        consistency_level="Strong"
     )
-    return res
+
+    # get the IDs of all returned hits
+    #results[0].ids
+
+    # get the distances to the query vector from all returned hits
+    #results[0].distances
+    print(results)
+
+    # get the value of an output field specified in the search request.
+    hit = results[0][0]
+    hit.entity.get('sentence')
+    return hit
