9a7176b36241be24f59be8b1b816d1724ea1f3f9
Isaac Liu <ijyliu@uchicago.edu>
2024-04-11 20:27:55 -0700

    separate out index creation code

--- Code/Load Embeddings and Create Index/create_L2_index.py
+++ Code/Load Embeddings and Create Index/create_L2_index.py
@@ -1,11 +1,12 @@
-#simple query testing, milvus is trash at this tho its good at vector similarity lookup
-from pymilvus import connections, utility, Collection
+# Create L2 (Euclidean distance) index for the collection.
+
+from pymilvus import connections, Collection
 
 connections.connect(host='localhost', port='19530')
 
 collection_name = 'text_embeddings'
 collection = Collection(name=collection_name)
-# literally no idea what this does but you need it
+# Create Euclidean L2 index
 index_params = {
     "metric_type": "L2",
     "index_type": "IVF_FLAT",
@@ -13,12 +14,3 @@ index_params = {
 }
 collection.create_index(field_name="embedding", index_params=index_params)
 collection.load()
-
-expr = f"company_name == 'TheHersheyCompany'"
-
-results = collection.query(expr=expr)
-
-for result in results:
-    print(result)
-
-    
